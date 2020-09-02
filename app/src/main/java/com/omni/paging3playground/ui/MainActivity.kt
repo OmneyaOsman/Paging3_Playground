b99e273a198044d5a38e1b159bf4f48a55f001fb
Omni <omneya.osman@outlook.com>
2020-09-03 01:21:46 +0200

    Initial commit

---
+++ app/src/main/java/com/omni/paging3playground/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.omni.paging3playground.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.omni.paging3playground.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
